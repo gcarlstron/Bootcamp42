78c8f9c238c24eb013bfe7c4b2033518700c4225
gpacheco <gpacheco@student.42sp.org.br>
2021-07-21 16:51:18 +0000

    adicionado C00

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+void	myprint(char x, char y, char z);
+
+void	ft_print_comb(void)
+{
+	char	x;
+	char	y;
+	char	z;
+
+	x = '0';
+	while (x <= '7')
+	{
+		y = x + 1;
+		while (y <= '8')
+		{
+			z = y + 1;
+			while (z <= '9')
+			{
+				myprint(x, y, z);
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
+}
+
+void	myprint(char x, char y, char z)
+{
+	write(1, &x, 1);
+	write(1, &y, 1);
+	write(1, &z, 1);
+	if (x<7)
+		write(1, ", ", 2);
+}
