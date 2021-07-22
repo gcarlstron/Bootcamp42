11753154f6d6ded8968dbcf0944dde9ff9719e60
gpacheco <gpacheco@student.42sp.org.br>
2021-07-22 18:15:26 +0000

    adicionado ex07 e adicionado cabeçario em todos

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -2,33 +2,32 @@
 
 void	myprint(int n1, int n2)
 {
-	char n[5];
+	char	n[5];
 
-	n[0] = '0'+n1/10;
-	n[1] = '0'+n1%10;
+	n[0] = '0' + n1 / 10;
+	n[1] = '0' + n1 % 10;
 	n[2] = ' ';
-	n[3] = '0'+n2/10;
-	n[4] = '0'+n2%10;
+	n[3] = '0' + n2 / 10;
+	n[4] = '0' + n2 % 10;
 	write (1, &n, sizeof(n));
 }
 
 void	ft_print_comb2(void)
 {
 	int	n1;
-	int n2;
+	int	n2;
 
 	n1 = 0;
 	while (n1 <= 98)
 	{
-		n2 = n1+1;
+		n2 = n1 + 1;
 		while (n2 <= 99)
 		{
-			myprint(n1,n2);
+			myprint(n1, n2);
 			if (n1 < 98)
-				write(1,", ",2);
+				write(1, ", ", 2);
 			n2++;
 		}
 		n1++;
 	}
-	
 }
