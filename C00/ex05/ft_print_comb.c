221cf5d04f2893a7c8d755251ab7ded6dfe84b1d
gpacheco <gpacheco@student.42sp.org.br>
2021-07-24 15:48:23 +0000

    corrigida a comparação para char == char

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -6,7 +6,7 @@
 /*   By: gpacheco <gpacheco@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 18:03:30 by gpacheco          #+#    #+#             */
-/*   Updated: 2021/07/22 18:03:37 by gpacheco         ###   ########.fr       */
+/*   Updated: 2021/07/24 15:47:27 by gpacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,6 @@ void	myprint(char x, char y, char z)
 	write(1, &x, 1);
 	write(1, &y, 1);
 	write(1, &z, 1);
-	if (x < 7)
+	if (x < '7')
 		write(1, ", ", 2);
 }
