6a748990ea26e4d5861f3ccc492eb1c183dbd709
gpacheco <gpacheco@student.42sp.org.br>
2021-07-24 16:28:09 +0000

    corrigido arquivos negativos

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: gpacheco <gpacheco@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 17:59:07 by gpacheco          #+#    #+#             */
-/*   Updated: 2021/07/22 18:02:24 by gpacheco         ###   ########.fr       */
+/*   Updated: 2021/07/24 13:14:25 by gpacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,26 @@ void	ft_putnbr(int nb)
 {
 	int	u;
 	int	d;
+	int	n;
 
-	u = nb % 10;
-	d = nb / 10;
+	n = 0;
 	if (nb < 0)
 	{
+		n = 1;
+		nb++;
 		nb = -nb;
 		write(1, "-", 1);
 	}
+	d = nb / 10;
 	if (d < 10 && nb >= 10)
 	{
 		d = '0' + d;
 		write(1, &d, 1);
 	}
 	else if (d > 0)
+		{
 		ft_putnbr(d);
-	u = '0' + nb % 10;
+		}
+	u = '0' + nb % 10 + n;
 	write(1, &u, 1);
 }
